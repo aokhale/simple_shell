c5b35998b6a5deeddfc16b5e0080ab9352b22057
oseokhale <oseokhalee@gmail.com>
2023-05-11 04:28:51 -0700

    By Okhale

--- _atoi.c
+++ _atoi.c
@@ -7,10 +7,12 @@
  * Return: 1 if interactive mode, 0 otherwise
  */
 
+
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
+
 
 /**
  * is_delim - checks if character is a delimeter
@@ -18,6 +20,7 @@ int interactive(info_t *info)
  * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
+
 
 int is_delim(char c, char *delim)
 {
@@ -27,11 +30,13 @@ int is_delim(char c, char *delim)
 	return (0);
 }
 
+
 /**
  *_isalpha - checks for alphabetic character
  *@c: The character to input
  *Return: 1 if c is alphabetic, 0 otherwise
  */
+
 
 int _isalpha(int c)
 {
@@ -41,15 +46,18 @@ int _isalpha(int c)
 		return (0);
 }
 
+
 /**
  *_atoi - converts a string to an integer
  *@s: the string to be converted
  *Return: 0 if no numbers in string, converted number otherwise
  */
 
+
 int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
+
 	unsigned int result = 0;
 
 	for (i = 0;  s[i] != '\0' && flag != 2; i++)
@@ -60,16 +68,23 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			flag = 1;
+
 			result *= 10;
+
 			result += (s[i] - '0');
 		}
+
 		else if (flag == 1)
+
 			flag = 2;
 	}
 
 	if (sign == -1)
+
 		output = -result;
+
 	else
+
 		output = result;
 
 	return (output);
