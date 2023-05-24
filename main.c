047d897d377e2dc97a3077ddb86cc20d0e0179cb
Emmanuel Sunday <emmanuelsunday2500@gmail.com>
2023-05-24 10:10:45 -0700

    updated main.c

--- main.c
+++ main.c
@@ -14,9 +14,9 @@ int main(int ac, char **av)
 	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
-			"add $3, %0"
-			: "=r" (fd)
-			: "r" (fd));
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
 	if (ac == 2)
 	{
@@ -43,4 +43,3 @@ int main(int ac, char **av)
 	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
-
